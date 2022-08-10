acab0ba4e7204c58e9746cffdf6224ad54d1fb25
Max Bye <mbye@techsoup.org>
2022-08-10 13:48:27 +0100

    addign side panels

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { useEffect, useRef, useState } from "react";
 import IframeResizer from 'iframe-resizer-react'
 import { GridStack } from 'gridstack';
 import 'gridstack/dist/gridstack.css';
+import './custom.css';
 
 import "./App.css";
 
 function App() {
+
+    const handleClick = (e) => {
+        if (e.type === 'click') {
+            console.log('Left click');
+        } else if (e.type === 'contextmenu') {
+            console.log('Right click');
+        }
+    };
+
     // Initialize Gridstack inside useEffect so that DOM is rendered when its initialized
     useEffect(() => {
         var grid = GridStack.init();
@@ -26,18 +36,94 @@ function App() {
 
     return (
         <div className="App">
+
+            {/* SIDE PANEL */}
+            <div className="sidePanel">
+                
+                        <div
+                            className="panels"
+                        >
+                            <iframe
+                                title="node"
+                                scrolling="no"
+                                src="http://127.0.0.1:5501/src/test_screens/nodes.html"
+                                style={{
+                                    border: 0,
+                                    pointerEvents: 'none',
+                                    height: '627px',
+                                    transformOrigin: 'top left',
+                                    transform: 'scale(.5,.5)',
+                                    marginTop: '-105px',
+                                }}
+                            >
+                            </iframe>
+                        </div>
+                
+                        <div
+                            className="panels"
+                        >
+                            <iframe
+                                title="node"
+                                scrolling="no"
+                                src="http://127.0.0.1:5501/src/test_screens/nodes.html"
+                                style={{
+                                    border: 0,
+                                    pointerEvents: 'none',
+                                    height: '627px',
+                                    transformOrigin: 'top left',
+                                    transform: 'scale(.5,.5)',
+                                    marginTop: '-105px',
+                                }}
+                            >
+                            </iframe>
+                        </div>
+
+
+
+            </div>
+            {/* side panel */}
+
+            {/* HEADER */}
+            <nav class="navbar navbar-fixed-top nav-one DPOFix margin-live">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="/"></a>
+                    <div class="dashboardContainer">
+                        <p >Dashboard</p>
+                    </div>
+
+                </div>
+
+                <div>
+                    <div id="signIn" class="navbar-corner">
+                        <div class="flex-buttons">
+                            <div class="btn btn-black-white" id="SignInID">
+                                Add
+                            </div>
+                            <div class="btn btn-black-white-inverse testCSS" id="JoinInID">
+                                Delete
+                            </div>
+                        </div>
+                    </div>
+                    <div id="joinIn" class="navbar-corner">
+
+                    </div>
+                </div>
+            </nav>
+            {/* header */}
+
+            {/* <p onClick={handleClick} onContextMenu={handleClick}>Something</p> */}
             <p>
                 https://authex-0.tsgctp.org:9005/services/capps/access
                 a-su3@ctpdev.org
                 a-su3@$435Brannan/3000
             </p>
 
-            <div id="loginPage" style={{ height: '100vh', width: '100vw', position: 'absolute', zIndex: '10', background: 'white' }}>
-                <IframeResizer onMessage={onMessage} style={{ height: '100vh', width: '100vw' }} src="http://127.0.0.1:5500/capps.html"></IframeResizer> 
-        </div>
+            {/* <div id="loginPage" style={{ height: '100vh', width: '100vw', position: 'absolute', zIndex: '10', background: 'white' }}>
+                <IframeResizer onMessage={onMessage} style={{ height: '100vh', width: '100vw' }} src="http://127.0.0.1:5500/capps.html"></IframeResizer>
+            </div> */}
 
-            <div className="grid-stack" style={{ height: '480px' }}>
-                <div className="grid-stack-item border-dark" data-gs-width="4" data-gs-height="7" style={{ height: '480px', width: '33%' }}>
+            <div className="grid-stack">
+                <div className="grid-stack-item border-dark" data-gs-width="4" data-gs-height="3">
                     <div className="grid-stack-item-content">
                         <div
                             style={{
@@ -45,13 +131,40 @@ function App() {
                             }}
                         >
                             <iframe
+                                title="node"
                                 scrolling="no"
-                                src="http://127.0.0.1:5500/node.html"
+                                src="http://127.0.0.1:5501/src/test_screens/nodes.html"
                                 style={{
                                     pointerEvents: 'none',
-                                    height: '1200px',
-                                    marginTop: '-120px',
-                                    marginLeft: '-30px',
+                                    height: '627px',
+                                    // transformOrigin: 'top left',
+                                    // transform: 'scale(2,2)',
+                                    marginTop: '-195px',
+                                    marginLeft: '-27px',
+                                    marginRight: '30px',
+                                    width: '34vw'
+                                }}
+                            >
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="grid-stack-item border-dark" data-gs-width="4" data-gs-height="3">
+                    <div className="grid-stack-item-content">
+                        <div
+                            style={{
+                                overflow: 'hidden',
+                            }}
+                        >
+                            <iframe
+                                title="node"
+                                scrolling="no"
+                                src="http://127.0.0.1:5501/src/test_screens/iworkslineitemimports.html"
+                                style={{
+                                    pointerEvents: 'none',
+                                    height: '635px',
+                                    marginTop: '-180px',
+                                    marginLeft: '-27px',
                                     marginRight: '30px',
                                     width: '38vw'
                                 }}
@@ -60,7 +173,7 @@ function App() {
                         </div>
                     </div>
                 </div>
-                <div className="grid-stack-item border-dark" data-gs-width="4" data-gs-height="7" style={{ height: '480px', width: '33%' }}>
+                <div className="grid-stack-item border-dark" data-gs-width="4" data-gs-height="3">
                     <div className="grid-stack-item-content">
                         <div
                             style={{
@@ -68,13 +181,14 @@ function App() {
                             }}
                         >
                             <iframe
+                                title="node"
                                 scrolling="no"
-                                src="http://127.0.0.1:5500/testest.html"
+                                src="http://127.0.0.1:5501/src/test_screens/orgcountbycountry.html"
                                 style={{
                                     pointerEvents: 'none',
-                                    height: '1200px',
-                                    marginTop: '-120px',
-                                    marginLeft: '-30px',
+                                    height: '615px',
+                                    marginTop: '-180px',
+                                    marginLeft: '-27px',
                                     marginRight: '30px',
                                     width: '38vw'
                                 }}
@@ -82,30 +196,7 @@ function App() {
                             </iframe>
                         </div>
                     </div>
-                </div>
-                <div className="grid-stack-item border-dark" data-gs-width="4" data-gs-height="7" style={{ height: '480px', width: '33%' }}>
-                    <div className="grid-stack-item-content">
-                        <div
-                            style={{
-                                overflow: 'hidden',
-                            }}
-                        >
-                            <iframe
-                                scrolling="no"
-                                src="http://127.0.0.1:5500/yoyo.html"
-                                style={{
-                                    pointerEvents: 'none',
-                                    height: '1200px',
-                                    marginTop: '-120px',
-                                    marginLeft: '-30px',
-                                    marginRight: '30px',
-                                    width: '38vw'
-                                }}
-                            >
-                            </iframe>
-                        </div>
-                    </div>
-                </div>
+                </div> */}
             </div>
         </div>
     );
