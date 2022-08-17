b9003d3b8cbee6b6a7df95780a20cebf6c0164e2
Max Bye <mbye@techsoup.org>
2022-08-17 10:47:32 +0100

    created delete space

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     let grid;
     useEffect(() => {
         grid = GridStack.init();
-        document.getElementById("nodePreview").addEventListener("click", addNode);
+        // document.getElementById("nodePreview").addEventListener("click", addNode);
 
         const toggleNodesInteractivity = (state) => {
             const nodes = document.getElementsByClassName('nodesContainer');
@@ -43,21 +43,7 @@ function App() {
 
     });
 
-    const [panelState, setPanelState] = useState('none');
-    const [editMode, toggleEditMode] = useState(false);
-    const [canDelete, toggleDeleteMode] = useState(false);
-
-    const intDeleteMode = () => {
-        setPanelState('remove nodes');
-        toggleDeleteMode(true);
-        toggleEditMode(!editMode);
-    }
-
-    const togglePanel = (addMode = false) => {
-        setPanelState('add nodes');
-        toggleDeleteMode(false);
-        toggleEditMode(!editMode);
-    }
+    const [dashboardState, setDashboardState] = useState('none');
 
     const [previewState, togglePreviewState] = useState({
         animation: true,
@@ -65,7 +51,7 @@ function App() {
     });
 
     const deleteNode = () => {
-        if (!canDelete) {
+        if (dashboardState !== 'remove nodes') {
             return;
         }
         // delete node
@@ -79,7 +65,7 @@ function App() {
         // add node to screen
         var items = [
             // {content: 'my first node'}, // will default to location (0,0) and 1x1
-            {id: 'nodesGraph', width: 3, height: 3, content: '<iframe class="nodesContainer" title="nodesGraph" id="nodesGraph" onclick="() => { console.log(1) }" src="http://127.0.0.1:5501/src/test_screens/1.html"></iframe>'},
+            { id: 'nodesGraph', width: 3, height: 3, content: '<iframe class="nodesContainer" title="nodesGraph" id="nodesGraph" onclick="() => { console.log(1) }" src="http://127.0.0.1:5501/src/test_screens/1.html"></iframe>' },
         ];
         // var grid = GridStack.init();
         grid.load(items);
@@ -99,82 +85,93 @@ function App() {
                 className="sidePanel"
                 animationIn="bounceInRight"
                 animationOut="bounceOutRight"
-                isVisible={panelState !== 'none'}
+                isVisible={dashboardState !== 'none'}
             >
-                <Animated
-                    className={`panels ${!previewState.preview ? "shrunk-preview" : ""}`}
-                    animationOut="zoomOut"
-                    isVisible={previewState.animation}
-                >
-                    <div id="nodePreview">
-                        <iframe
-                            onClick={addNode}
-                            title="node"
-                            scrolling="no"
-                            src="http://127.0.0.1:5501/src/test_screens/1.html"
-                            style={{
-                                pointerEvents: 'none',
-                                border: 0,
-                                height: '627px',
-                                transformOrigin: 'top left',
-                                transform: 'scale(.5,.5)',
-                                marginTop: '-105px',
-                            }}
+                {/*
+                * ADD NODES
+                */}
+                {(dashboardState === 'add nodes' &&
+                    <section>
+                        <Animated
+                            className={`panels ${!previewState.preview ? "shrunk-preview" : ""}`}
+                            animationOut="zoomOut"
+                            isVisible={previewState.animation}
                         >
-                        </iframe>
-                    </div>
-                </Animated>
+                            <div id="nodePreview" onClick={addNode}>
+                                <iframe
+                                    onClick={addNode}
+                                    title="node"
+                                    scrolling="no"
+                                    src="http://127.0.0.1:5501/src/test_screens/1.html"
+                                    style={{
+                                        pointerEvents: 'none',
+                                        border: 0,
+                                        height: '627px',
+                                        transformOrigin: 'top left',
+                                        transform: 'scale(.5,.5)',
+                                        marginTop: '-105px',
+                                    }}
+                                >
+                                </iframe>
+                            </div>
+                        </Animated>
+                        <div
+                            className="panels"
+                        >
+                            <iframe
+                                title="node"
+                                scrolling="no"
+                                src="http://127.0.0.1:5501/src/test_screens/2.html"
+                                style={{
+                                    border: 0,
+                                    pointerEvents: 'none',
+                                    height: '627px',
+                                    marginTop: '-105px',
+                                }}
+                            >
+                            </iframe>
+                        </div>
 
-                <div
-                    className="panels"
-                >
-                    <iframe
-                        title="node"
-                        scrolling="no"
-                        src="http://127.0.0.1:5501/src/test_screens/2.html"
-                        style={{
-                            border: 0,
-                            pointerEvents: 'none',
-                            height: '627px',
-                            marginTop: '-105px',
-                        }}
-                    >
-                    </iframe>
-                </div>
+                        <div
+                            className="panels"
+                        >
+                            <iframe
+                                title="node"
+                                scrolling="no"
+                                src="http://127.0.0.1:5501/src/test_screens/3.html"
+                                style={{
+                                    border: 0,
+                                    pointerEvents: 'none',
+                                    height: '627px',
+                                    marginTop: '-105px',
+                                }}
+                            >
+                            </iframe>
+                        </div>
 
-                <div
-                    className="panels"
-                >
-                    <iframe
-                        title="node"
-                        scrolling="no"
-                        src="http://127.0.0.1:5501/src/test_screens/3.html"
-                        style={{
-                            border: 0,
-                            pointerEvents: 'none',
-                            height: '627px',
-                            marginTop: '-105px',
-                        }}
-                    >
-                    </iframe>
-                </div>
-
-                <div
-                    className="panels"
-                >
-                    <iframe
-                        title="node"
-                        scrolling="no"
-                        src="http://127.0.0.1:5501/src/test_screens/4.html"
-                        style={{
-                            border: 0,
-                            pointerEvents: 'none',
-                            height: '627px',
-                            marginTop: '-105px',
-                        }}
-                    >
-                    </iframe>
-                </div>
+                        <div
+                            className="panels"
+                        >
+                            <iframe
+                                title="node"
+                                scrolling="no"
+                                src="http://127.0.0.1:5501/src/test_screens/4.html"
+                                style={{
+                                    border: 0,
+                                    pointerEvents: 'none',
+                                    height: '627px',
+                                    marginTop: '-105px',
+                                }}
+                            >
+                            </iframe>
+                        </div>
+                    </section>)}
+                {/*
+                * MOVE/REMOVE NODES
+                */}
+                {(dashboardState === 'remove nodes' &&
+                    <h1>DRAG HERE</h1>
+                )}
             </Animated>
             {/* side panel */}
 
@@ -190,16 +187,16 @@ function App() {
                 <div>
                     <div id="signIn" className="navbar-corner">
                         <div className="flex-buttons">
-                            {panelState !== 'none' && (<div className="btn btn-black-white" id="GoBack" onClick={() => { setPanelState('none'); }}>
+                            {dashboardState !== 'none' && (<div className="btn btn-black-white" id="GoBack" onClick={() => { setDashboardState('none'); }}>
                                 Done
                             </div>)}
-                            {panelState === 'none' && (
+                            {dashboardState === 'none' && (
                                 <>
-                                    <div className="btn btn-black-white" id="Add" onClick={() => { togglePanel(true) }}>
-                                        Add
+                                    <div className="btn btn-black-white" id="Add" onClick={() => { setDashboardState('add nodes') }}>
+                                        Add Nodes
                                     </div>
-                                    <div className="btn btn-black-white-inverse" id="Delete" onClick={intDeleteMode}>
-                                        Delete
+                                    <div className="btn btn-black-white-inverse" id="Delete" onClick={() => { setDashboardState('remove nodes') }}>
+                                        Move / Remove Nodes
                                     </div>
                                 </>
                             )}
